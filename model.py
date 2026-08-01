5f0fc2c868b9915ed85642d1971197e4e6222e30
Hami Ibriyamov <hamop3108@gmail.com>
2026-08-01 19:32:48 +0200

    Step 8: init_mlp_params

--- model.py
+++ model.py
@@ -82,11 +82,10 @@ def init_linear_layer(key, in_dim, out_dim, scale=0.1):
 def init_mlp_params(key, layer_sizes, scale=0.1):
     # TODO: build a list of per-layer parameter dicts from adjacent layer sizes.
     layers = []
+    # don't forget to split the prng key into prng keys for each layer 
+    key = split_prng_key(key, len(layer_sizes))
     for l in range(len(layer_sizes) - 1):
-        in_dim = layer_sizes[l]
-        out_dim = layer_sizes[l+1]
-        layers.append(init_linear_layer(key, in_dim, out_dim, scale=scale))
-
+        layers.append(init_linear_layer(key[l], in_dim=layer_sizes[l], out_dim=layer_sizes[l+1], scale=scale))
     return layers
 
 # Step 9 - linear_forward (not yet solved)

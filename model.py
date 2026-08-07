dadd3c35acb64fb188f8e507873bd0c57743ad7c
Hami Ibriyamov <hamop3108@gmail.com>
2026-08-07 12:55:38 +0200

    Step 20: train_mlp

--- model.py
+++ model.py
@@ -196,8 +196,14 @@ def training_step(params, x, one_hot_targets, learning_rate):
 
     return (updated_params, scalar_loss)
 
-# Step 20 - train_mlp (not yet solved)
-# TODO: implement
+# Step 20 - train_mlp
+def train_mlp(params, x, one_hot_targets, learning_rate, num_epochs):
+    """Run num_epochs full-batch SGD updates and return the final params."""
+    # TODO: run num_epochs full-batch SGD updates via training_step and return final params
+    for _ in range(num_epochs):
+        params, _ = training_step(params, x, one_hot_targets, learning_rate)
+
+    return params
 
 # Step 21 - predict_classes (not yet solved)
 # TODO: implement
